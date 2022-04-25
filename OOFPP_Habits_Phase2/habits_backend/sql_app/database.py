42686e1723428e37193a917ee4511aef2dca2967
Gerhard Maree <Gerhardus.Maree@sms-digital.com>
2022-04-25 16:24:36 +0200

    Setup SQLAlchemy model

--- OOFPP_Habits_Phase2/habits_backend/sql_app/database.py
+++ OOFPP_Habits_Phase2/habits_backend/sql_app/database.py
@@ -12,6 +12,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./habits_tracking.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+# Create a local database session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
---
+++ OOFPP_Habits_Phase2/habits_backend/sql_app/models.py
@@ -0,0 +1,26 @@
+"""This file contains the model declarations used for saving the data"""
+from sqlalchemy import DateTime, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from .database import Base
+
+
+class Habit(Base):
+    __tablename__ = "habits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+    creation_date = Column(DateTime,nullable=False,default=datetime.utcnow,comment="Creation date in UTC")
+    frequency_id=Column(Integer,ForeignKey("frequencies.id"))
+
+    frequency = relationship("Frequency", back_populates="habits")
+
+
+class Frequency(Base):
+    __tablename__ = "frequencies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    frequency = Column(String)  # %d or %m or %y
