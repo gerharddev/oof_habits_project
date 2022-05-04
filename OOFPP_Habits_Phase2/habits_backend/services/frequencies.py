176442053b35edf03fecdedb5ead8a7f2d45e9b1
Gerhard Maree <gerhardus.maree@sms-digital.com>
2022-05-04 18:30:56 +0200

    Return 404 for not found

--- OOFPP_Habits_Phase2/habits_backend/services/frequencies.py
+++ OOFPP_Habits_Phase2/habits_backend/services/frequencies.py
@@ -22,7 +22,7 @@ class FrequenciesService:
         with get_db() as session:
             db_frequencies = crud.get_frequencies(session)
         frequencies = [schemas.Frequency.from_orm(h) for h in db_frequencies]
-        # from_orm: loads data into a model from an arbitrary class
+
         return frequencies
 
     @classmethod
